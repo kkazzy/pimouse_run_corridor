0fdebd0a97015b6fce705432c5203c7ba574498a
Kazuyuki Kawazu <knz_soft@yahoo.co.jp>
2018-02-08 15:06:04 +0900

    update wall_stop.py 180208

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # pimouse_run_corridor
+Section 9 ROSでロボットを走らせる
+
+[!Build Status](https://travis-ci.org/kkazzy/pimouse_run_corridor.svg?branch=master)](https://travis-ci.org/kkazzy/pimouse_run_corridor)
+
--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -18,17 +18,17 @@ class WallStopTest(unittest.TestCase):
         return left, right
             
     def test_io(self):
-        left, right = self.set_and_get(400, 100, 100,   0) #total: 600
+        left, right = self.set_and_get(400,100,100,0) #total: 600
         self.assertTrue(left == 0 and right == 0, "can't stop")
 
-        left, right = self.set_and_get(400,   0,   0,  99) #total: 499
+        left, right = self.set_and_get(400,0,0,99) #total: 499
         self.assertTrue(left != 0 and right != 0, "can't move")
 
-        left, right = self.set_and_get(150,   0, 200, 150) #total: 500
+        left, right = self.set_and_get(150,0,200,150) #total: 500
         self.assertTrue(left == 0 and right == 0, "can't stop")
 
 
 if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_wall_stop')
-    rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop', 'WallStopTest')
+    rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop', WallStopTest)
